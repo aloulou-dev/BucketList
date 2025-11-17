95efaa1906ba43909ef4dab99d59cd855165d470
aloulou_dev <aloulou@bc.edu>
2025-11-17 16:31:49 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Malek Aloulou on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
